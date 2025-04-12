7345809d135bdd80b43bed8e1d345dd5990475f6
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2025-04-11 19:45:02 -0500

    style: set Roboto as global font

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,11 @@ import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const theme = createTheme();
+const theme = createTheme({
+  typography: {
+    fontFamily: "Robot, sans-serif",
+  },
+});
 root.render(
   <ApolloProvider client={client}>
     <ThemeProvider theme={theme}>
